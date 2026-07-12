49cd6de25e8c7b7efba6a254fb45af89fd18e371
tarundeepakjain <tarundeepakjain@gmail.com>
2026-07-12 11:14:29 +0530

    Implement Maintenance module and update app.js routing

--- server/src/app.js
+++ server/src/app.js
@@ -4,6 +4,7 @@ import dotenv from "dotenv";
 import { errorHandler } from "./middlewares/errorHandler.js";
 import vehicleRoutes from "./modules/vehicle/vehicle.routes.js";
 import authRouter from "./modules/auth/auth.routes.js";
+import maintenanceRoutes from "./modules/maintenance/maintenance.routes.js";
 
 dotenv.config();
 
@@ -19,6 +20,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/api/vehicles", vehicleRoutes);
+app.use("/api/maintenance", maintenanceRoutes);
 
 
 app.get("/health", (req, res) => {
