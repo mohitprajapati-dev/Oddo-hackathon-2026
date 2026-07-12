2d6245339ad37a50ce595820597f4e64faee2431
tarundeepakjain <tarundeepakjain@gmail.com>
2026-07-12 13:39:26 +0530

    Allow POST /api/vehicles to resolve newVehicle endpoint for client alignment

--- server/src/modules/vehicle/vehicle.routes.js
+++ server/src/modules/vehicle/vehicle.routes.js
@@ -6,6 +6,7 @@ import { protect } from "../../middlewares/authMiddleware.js";
 const router = express.Router();
 
 router.get("/",protect,vehicleController.fetchVehiclesByOnwer);
+router.post("/", protect, vehicleController.newVehicle);
 router.post("/new", protect, vehicleController.newVehicle);
 router.post("/update-status",protect,vehicleController.updateVehicleStatus)
 router.post("/update-odometer",protect,vehicleController.updateOdometer)
